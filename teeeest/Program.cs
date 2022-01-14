4d4a3643ffd24dc03070d5097fc3eba7d3c58dcb
dawnce <dawnce@DAWNCE>
2022-01-14 15:42:06 +0700

    Добавьте файлы проекта.

---
+++ teeeest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace teeeest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
